1fbb17fbea63015cb75f306d26d4f03c9f6add83
Nancy Weng <wengalex55@gmail.com>
2025-02-07 09:26:01 -0500

    data 1

---
+++ note.py
@@ -0,0 +1,18 @@
+
+""" def login(password):
+    #if statement evaluates to true do next line
+    if password == "secret":
+        print("logged in")
+    else:
+        print("incorrect password")
+    Login("secrets") """
+
+def temp(x):
+    if x >80:
+         print("too hot")
+    elif x > 60:
+        print("nice")
+    else:
+        print("Too Cold")
+x = int(input("what da the temp"))
+temp(x)
