c4d14200e8b15db0ad0f9ddb1b2d1f27b64bc9a4
Nancy Weng <wengalex55@gmail.com>
2025-02-12 09:26:30 -0500

    data 5

--- data.py
+++ data.py
@@ -68,4 +68,8 @@ tip = calculate_tip(bill, service)
 print(tip) """
 
 def find_factors(number):  
-    
+    factors = []
+    for i in range(1, number + 1):
+        if number % i == 0:
+            factors.append(i)
+            return factors
